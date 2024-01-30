31183db8030a173655ad08a1b5b83c80be81cc24
joharehl <156836225+joharehl@users.noreply.github.com>
2024-01-30 19:53:35 +0000

    message to send

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,9 +2,9 @@ module.exports = {
 	title: "Eleventy Base Blog v8",
 	url: "https://example.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "I am writing about my experiences in web dev course.",
 	author: {
-		name: "Your Name Here",
+		name: "Joharé",
 		email: "youremailaddress@example.com",
 		url: "https://example.com/about-me/"
 	}
